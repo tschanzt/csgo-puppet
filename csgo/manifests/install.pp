6ecfef318435e8d919e424a81559a2d39dad0bf1
Timon Tschanz <timon.tschanz@gmail.com>
2019-11-16 14:45:51 +0100

    update for switzerlan

--- csgo/manifests/install.pp
+++ csgo/manifests/install.pp
@@ -39,15 +39,14 @@ class csgo::install (
         url => 'https://sm.alliedmods.net/smdrop/1.9/sourcemod-1.9.0-git6259-linux.tar.gz',
         src_target => '/tmp',
     }
-    archive { 'pugsetup':
+    archive { 'get5':
         user => 'eevent',
         checksum => false,
         target => "${game_directory}/csgo",
         ensure => present,
-        url => 'https://github.com/splewis/csgo-pug-setup/releases/download/2.0.5/pugsetup_2.0.5.zip',
+        url => 'https://github.com/splewis/get5/releases/download/0.7.1/get5_0.7.1.zip',
         follow_redirects => true,
         src_target => '/tmp',
-        strip_components => 1,
         extension => 'zip'
     }
 
@@ -80,29 +79,53 @@ class csgo::install (
         mode => '774'
     }
 
-    file {"${game_directory}/csgo/cfg/sourcemod/pugsetup/live.cfg":
+    file {"${game_directory}/csgo/cfg/sourcemod/get5/live.cfg":
         replace => true,
         source => 'puppet:///modules/csgo/live.cfg',
         owner => 'eevent',
         group => 'eevent',
-        require => Archive['pugsetup'],
+        require => Archive['get5'],
     }
 
-    file {"${game_directory}/csgo/addons/sourcemod/configs/pugsetup/maps.txt":
+    file {"${game_directory}/csgo/addons/sourcemod/cfg/get5.cfg":
         replace => true,
-        source => 'puppet:///modules/csgo/maps.txt',
+        source => 'puppet:///modules/csgo/get5.cfg',
         owner => 'eevent',
         group => 'eevent',
-        require => Archive['pugsetup'],
+        require => Archive['get5'],
+    }
+
+    file {"${game_directory}/csgo/addons/sourcemod/cfg/get5.cfg":
+        replace => true,
+        source => 'puppet:///modules/csgo/get5.cfg',
+        owner => 'eevent',
+        group => 'eevent',
+        require => Archive['get5'],
+    }
+
+    file {"${game_directory}/csgo/bo1.cfg":
+        replace => true,
+        source => 'puppet:///modules/csgo/bo1.cfg',
+        owner => 'eevent',
+        group => 'eevent',
+        require => Archive['get5'],
+    }
+
+    file {"${game_directory}/csgo/bo3.cfg":
+        replace => true,
+        source => 'puppet:///modules/csgo/bo3.cfg',
+        owner => 'eevent',
+        group => 'eevent',
+        require => Archive['get5'],
     }
 
     $codefile = $::hostname?{
-    'eevent'=> file('csgo/eevent-csgo-1.txt'),
-    'eevent-exdns-1'=> file('csgo/eevent-csgo-2.txt'),
-    'eevent1'=> file('csgo/eevent-csgo-3.txt'),
-    'eevent0'=> file('csgo/eevent-csgo-4.txt'),
-    'eevent3'=> file('csgo/eevent-csgo-5.txt'),
-    'eevent4'=> file('csgo/eevent-csgo-6.txt'),
+    'eevent1'=> file('csgo/eevent-csgo-1.txt'),
+    'eevent2'=> file('csgo/eevent-csgo-2.txt'),
+    'eevent3'=> file('csgo/eevent-csgo-3.txt'),
+    'eevent4'=> file('csgo/eevent-csgo-4.txt'),
+    'eevent5'=> file('csgo/eevent-csgo-5.txt'),
+    'eevent6'=> file('csgo/eevent-csgo-6.txt'),
 }
     $codes = $codefile.split('\n')
 
